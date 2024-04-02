1281865de85a6a34f7d8fc5e15e29267d6e29d5b
Hugo Josefson <hugo@josefson.org>
2024-04-02 23:28:15 +0200

    chore: delete unneeded files

--- test/placeholder.test.ts
+++
@@ -1,8 +0,0 @@
-import { describe, it } from "https://deno.land/std@0.221.0/testing/bdd.ts";
-import { placeholder } from "../mod.ts";
-
-describe("placeholder", () => {
-  it("should not throw", () => {
-    placeholder();
-  });
-});
